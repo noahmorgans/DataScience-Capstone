5b1efebef75de606dd86ba00e5cac8fcfb69fc89
unknown <noahmorgans24@gmail.com>
2026-03-14 18:42:57 -0500

    defined, calculated, and added momentum stats for each year

--- CombineDataScript.py
+++ CombineDataScript.py
@@ -1,3 +1,5 @@
+import sys
+import os
 import pandas as pd
 import re
 
@@ -39,18 +41,33 @@ def parse_game_line(line):
         "team2_result": team2_result
     }
 
+def game_in_first_half(team, date, dates_dict):
+
+    N = len(dates_dict[team])
+    k = dates_dict[team].index(date) + 1
+    midpoint = (N + 1) / 2
+    
+    if k < midpoint:
+        return -1
+    elif k == midpoint:
+        return 0
+    else:
+        return 1
+
+YEAR = 2008
+
 stats_df = pd.read_csv(
-    "PreTournamentStats/2025.csv",
-    encoding="cp1252"
+    f"PreTournamentStats/{YEAR}.csv",
+    encoding="utf-8-sig"
 )
 
-print(stats_df)
+num_teams = len(stats_df)
 
 gameDict = {}
 
 records = []
 
-with open("PreTournamentGameLogs/2025.txt", encoding="utf-8", errors="ignore") as f:
+with open(f"PreTournamentGameLogs/{YEAR}.txt", encoding="utf-8", errors="ignore") as f:
     for line in f:
         record = parse_game_line(line)
         if record is not None:
@@ -77,17 +94,53 @@ for i, row in games_df.iterrows():
         games_df.at[i, key] = value
 
 games_df = games_df[games_df["team1_rank"].notna() & games_df["team2_rank"].notna()]
+
+dates_dict = {}
+
+for i, row in games_df.iterrows():
+    team1 = row["team1"]
+    team2 = row["team2"]
+
+    if team1 not in dates_dict:
+        dates_dict[team1] = []
+    if team2 not in dates_dict:
+        dates_dict[team2] = []
     
-stats_df["rating"] = 0.0
-stats_df["AdjEM"] = stats_df["AdjOE"] - stats_df["AdjDE"]
-stats_df["EFG%M"] = stats_df["EFG%"] - stats_df["EFGD%"]
-stats_df["TORM"] = stats_df["TOR"] - stats_df["TORD"]
-stats_df["FTRM"] = stats_df["FTR"] - stats_df["FTRD"]
-stats_df["TRB"] = stats_df["ORB"] + stats_df["DRB"]
-stats_df["FTRM"] = stats_df["FTR"] - stats_df["FTRD"]
-stats_df["3P%M"] = stats_df["3P%"] - stats_df["3P%D"]
-stats_df["2P%M"] = stats_df["2P%"] - stats_df["2P%D"]
-stats_df["3PRM"] = stats_df["3PR"] - stats_df["3PRD"]
+    dates_dict[team1].append(row["date"])
+    dates_dict[team2].append(row["date"])
+
+# stats_df["AdjEM"] = stats_df["AdjOE"] - stats_df["AdjDE"]
+# stats_df["EFG%M"] = stats_df["EFG%"] - stats_df["EFGD%"]
+# stats_df["TORM"] = stats_df["TOR"] - stats_df["TORD"]
+# stats_df["FTRM"] = stats_df["FTR"] - stats_df["FTRD"]
+# stats_df["TRB"] = stats_df["ORB"] + stats_df["DRB"]
+# stats_df["FTRM"] = stats_df["FTR"] - stats_df["FTRD"]
+# stats_df["3P%M"] = stats_df["3P%"] - stats_df["3P%D"]
+# stats_df["2P%M"] = stats_df["2P%"] - stats_df["2P%D"]
+# stats_df["3PRM"] = stats_df["3PR"] - stats_df["3PRD"]
+
+stats_df["First_Half_Potential"] = 0.0
+stats_df["Second_Half_Potential"] = 0.0
+
+for i, row in games_df.iterrows():
+    team1 = row["team1"]
+    team2 = row["team2"]
+    
+    team1_potential_change = (num_teams - stats_df.loc[team2, "Rk"]) / num_teams
+    team2_potential_change = (num_teams - stats_df.loc[team1, "Rk"]) / num_teams
+
+    if game_in_first_half(team1, row["date"], dates_dict) == -1:
+        stats_df.loc[team1, "First_Half_Potential"] += team1_potential_change
+    elif game_in_first_half(team1, row["date"], dates_dict) == 1:
+        stats_df.loc[team1, "Second_Half_Potential"] += team1_potential_change
+
+    if game_in_first_half(team2, row["date"], dates_dict) == -1:
+        stats_df.loc[team2, "First_Half_Potential"] += team2_potential_change
+    elif game_in_first_half(team2, row["date"], dates_dict) == 1:
+        stats_df.loc[team2, "Second_Half_Potential"] += team2_potential_change
+
+stats_df["First_Half_Momentum"] = 0.0
+stats_df["Second_Half_Momentum"] = 0.0
 
 for i, row in games_df.iterrows():
     team1 = row["team1"]
@@ -108,12 +161,23 @@ for i, row in games_df.iterrows():
     loser_rank = stats_df.loc[loser, "Rk"]
 
     # Compute rating changes
-    loser_change = (winner_rank - 1) / 364
-    winner_change = (364 - loser_rank) / 364
+    loser_change = (winner_rank - 1) / num_teams
+    winner_change = (num_teams - loser_rank) / num_teams
 
-    # Update running totals
-    stats_df.loc[loser, "rating"] -= loser_change
-    stats_df.loc[winner, "rating"] += winner_change
+    if game_in_first_half(winner, row["date"], dates_dict) == -1:
+        stats_df.loc[winner, "First_Half_Momentum"] += winner_change
+    elif game_in_first_half(winner, row["date"], dates_dict) == 1:
+        stats_df.loc[winner, "Second_Half_Momentum"] += winner_change
+    if game_in_first_half(loser, row["date"], dates_dict) == -1:
+        stats_df.loc[loser, "First_Half_Momentum"] -= loser_change
+    elif game_in_first_half(loser, row["date"], dates_dict) == 1:
+        stats_df.loc[loser, "Second_Half_Momentum"] -= loser_change
+
+stats_df["Momentum"] = (stats_df["Second_Half_Momentum"]/stats_df["Second_Half_Potential"]) - (stats_df["First_Half_Momentum"]/stats_df["First_Half_Potential"])
 
 print(games_df)
-print(stats_df)
+print(stats_df['Momentum'])
+
+os.makedirs("Momentum", exist_ok=True)
+stats_df = stats_df.reset_index()[['Team', 'Momentum']]
+stats_df.to_csv(f"Momentum/{YEAR}.csv", index=False)
